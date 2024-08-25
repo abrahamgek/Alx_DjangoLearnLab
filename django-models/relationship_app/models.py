158de48e6424bbd70f8300c8f90a1da1b18facfc
abrahamgek <girmaa065@gmail.com>
2024-08-25 19:02:41 +0000

    update

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -2,12 +2,12 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-
+from django.dispatch import sender
 
 class UserProfile(models.Model):
     ROLE_CHOICES = [
         ('Admin', 'Admin'),
-        ('Librarian', 'Librarian'),
+        ('Librarian', 'Librarians'),
         ('Member', 'Member'),
     ]
     user = models.OneToOneField(User, on_delete=models.CASCADE)
