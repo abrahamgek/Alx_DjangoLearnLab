272fc07d6b47edd2c822f5890ff7e27fba72687c
abrahamgek <girmaa065@gmail.com>
2024-08-25 19:26:11 +0000

    update

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -50,3 +50,15 @@ class Librarian(models.Model):
 
     def __str__(self):
         return self.name
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    publication_year = models.IntegerField()
+
+    class Meta:
+        permissions = [
+            ("can_add_book", "Can add book"),
+            ("can_change_book", "Can change book"),
+            ("can_delete_book", "Can delete book"),
+        ]
--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -6,7 +6,12 @@ from .views import list_books
 from .views import admin_view
 from .views import librarian_view
 from .views import member_view
+from .views import add_book, edit_book, delete_book
+
 urlpatterns = [
+    path('books/add/', add_book, name='add_book'),
+    path('books/edit/<int:pk>/', edit_book, name='edit_book'),
+    path('books/delete/<int:pk>/', delete_book, name='delete_book'),
     path('books/', list_books, name='list_books'),
     path('library/<int:pk>/', LibraryDetailView.as_view(template_name = 'relationship_app/library_detail.html'), name='library_detail'),
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
