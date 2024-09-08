e23fffdd6205102fdf4a703bcfdec49504c812b7
abrahamgek <girmaa065@gmail.com>
2024-09-08 19:26:02 +0000

    update

--- api_project/api/serializers.py
+++ api_project/api/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from .models import Book
 
-class BookSerializer(generics.ListAPIView):
+class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = '__all__'
