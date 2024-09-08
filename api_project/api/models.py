04716acb2a3d9b9afdb3edfc7758284540290a8e
abrahamgek <girmaa065@gmail.com>
2024-09-08 19:15:46 +0000

    update

--- api_project/api/models.py
+++ api_project/api/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from rest_framework import serializers
 class Book(models.Model):
     title = models.CharField(max_length=100)
     author = models.CharField(max_length=100)
--- api_project/api/serializers.py
+++ api_project/api/serializers.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
-
+from rest_framework import serializers
 
 class ApiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
