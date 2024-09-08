79f69977785a281b2c6299bd6a8c9f61a1ee7498
abrahamgek <girmaa065@gmail.com>
2024-09-08 19:38:42 +0000

    update

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -9,7 +9,17 @@
 from rest_framework import viewsets
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import BasePermission
+
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
+    
+class IsAdminOrReadOnly(BasePermission):
+    def has_permission(self, request, view):
+        if request.method in SAFE_METHODS:
+            return True
+        return request.user and request.user.is_staff
